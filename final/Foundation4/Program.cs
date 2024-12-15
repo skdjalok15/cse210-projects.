804115ea9f368e128d291e565f3261016bdad7b8
skdjalok15 <skdjalok15@gmail.com>
2024-12-15 07:08:34 +0530

    Update Program.cs

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        // Create activities
+        Activity running = new Running("03 Nov 2022", 30, 3.0);
+        Activity cycling = new Cycling("04 Nov 2022", 40, 15.0);
+        Activity swimming = new Swimming("05 Nov 2022", 25, 20);
+
+        // Add to a list
+        List<Activity> activities = new List<Activity> { running, cycling, swimming };
+
+        // Display summaries
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
