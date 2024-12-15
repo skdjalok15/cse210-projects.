5e899ea334f5e24f802f48a4370129687d35f0dd
skdjalok15 <skdjalok15@gmail.com>
2024-12-15 07:06:55 +0530

    Create Running.cs

---
+++ final/Foundation4/Running.cs
@@ -0,0 +1,25 @@
+public class Running : Activity
+{
+    private double distance;
+
+    public Running(string date, int minutes, double distance) 
+        : base(date, minutes)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+}
